6eca23c93ed2286cc4121a01c36281d269715e22
TylerGlenski <40610184+TylerGlenski@users.noreply.github.com>
2018-08-23 21:13:59 -0500

    Create problemOneAnswer.py

---
+++ CodingProblems/problemOneAnswer.py
@@ -0,0 +1,32 @@
+
+'''
+Given a list and an integer k, remove every kth  element from the list.
+k is guaranteed to be smaller than the length of the list.
+
+The list is very long
+Do this in one pass.
+'''
+### My Answer ###
+#create a test list here
+test_list = ['is', 'guaranteed', 'to', 'be',
+'smaller', 'than', 'the', 'length',
+'of', 'the', 'list', 'index',
+'variable', 'to', 'keep', 'logic'] # string test  #[i for i in range(11)] #integer test
+    
+def remove_kth_element(inputList, k):
+    
+    index = 0 
+    
+    #modify loop here
+    for i in inputList:
+        index += 1
+        if index % k == 0:
+            inputList.pop(index)
+            
+    #fixes element[1] remaining when indexes start at 0
+    if k == 2:
+        inputList.pop(1);
+        
+    return inputList
+
+print(remove_kth_element(test_list,2))
