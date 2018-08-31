61aa236cdef4088891bf6de7093fe426d72b60ca
TylerGlenski <40610184+TylerGlenski@users.noreply.github.com>
2018-08-30 19:53:44 -0500

    Update word_list_gen.py

--- projects_python/word_list_gen.py
+++ projects_python/word_list_gen.py
@@ -1,13 +1,30 @@
 ### Will create a word list based off a string. 
-# goal is to add this to a larger program or 'object' 
+# goal is to add this to a larger program
+
+## test string ##
+    txt = ''''''
+    txt += '''When you have mastered the Way of strategy, you can suddenly make your body like a rock, and ten thousand things cannot touch you '''
+    txt += '''Therefore the best fortress is to be found in the love of the people, for although you have fortresses, '''
+    txt += '''they will not save you if you are hated by the people. - Machevelli '''
+    txt += '''But afterwards they were of little use to her when Cesare Borgia attacked her '''
+    txt += ''' and the people being hostile to her allied themselves with the foreigner.'''
+    txt += ''' So that then and before it would have been safer for her not to have been hated by the people than to have had the fortresses.'''
+    txt += '''He considered where he could get such experience. '''
+    txt += '''There was the Musashi Flex, a loosely-organized band of modern ronins who travelled around challenging each other;  '''
+    txt +='''he could try that. Or, there was The Maze. '''
+    txt +='''Such a thing was risky, but it offered a real test. '''
+    txt +='''Injury was likely, death a possibility in the game known as The Maze; '''
+    txt += '''if he could survive that, maybe he would be ready.... '''
 
 def build_word_list(input_txt):
 ## input txt to be replaced with input from website/app ##
     txt = input_txt
     txt += '''When you have mastered the Way of strategy, you can suddenly make your body like a rock, and ten thousand things cannot touch you '''
     txt += '''Therefore the best fortress is to be found in the love of the people, for although you have fortresses, '''
-    txt += '''they will not save you if you are hated by the people. '''
-    txt += '''DEATH CAME FOR him wearing a smile '''
+    txt += '''they will not save you if you are hated by the people. - Machevelli '''
+    txt += '''But afterwards they were of little use to her when Cesare Borgia attacked her '''
+    txt += ''' and the people being hostile to her allied themselves with the foreigner.'''
+    txt += ''' So that then and before it would have been safer for her not to have been hated by the people than to have had the fortresses.'''
     txt += '''He considered where he could get such experience. '''
     txt += '''There was the Musashi Flex, a loosely-organized band of modern ronins who travelled around challenging each other;  '''
     txt +='''he could try that. Or, there was The Maze. '''
@@ -32,7 +49,7 @@ def build_word_list(input_txt):
     
     #takes the first list and makes new list without any words > 10 letters and none < 4 letters
     #customize final list here
-    word_list = [i for i in new_list if len(i) >= 4 and len(i) < 10]
+    word_list = [i for i in new_list if len(i) >= 4 and len(i) < 20]
     
     #return finished list
     return(word_list)
