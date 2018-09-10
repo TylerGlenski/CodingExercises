d7ce810b8e293d2a6cfd59d43d3a353328ea7d32
TylerGlenski <40610184+TylerGlenski@users.noreply.github.com>
2018-09-10 17:49:48 -0500

    removed dup test string

--- projects_python/word_list_gen.py
+++ projects_python/word_list_gen.py
@@ -19,18 +19,7 @@
 def build_word_list(input_txt):
 ## input txt to be replaced with input from website/app ##
     txt = input_txt
-    txt += '''When you have mastered the Way of strategy, you can suddenly make your body like a rock, and ten thousand things cannot touch you '''
-    txt += '''Therefore the best fortress is to be found in the love of the people, for although you have fortresses, '''
-    txt += '''they will not save you if you are hated by the people. - Machevelli '''
-    txt += '''But afterwards they were of little use to her when Cesare Borgia attacked her '''
-    txt += ''' and the people being hostile to her allied themselves with the foreigner.'''
-    txt += ''' So that then and before it would have been safer for her not to have been hated by the people than to have had the fortresses.'''
-    txt += '''He considered where he could get such experience. '''
-    txt += '''There was the Musashi Flex, a loosely-organized band of modern ronins who travelled around challenging each other;  '''
-    txt +='''he could try that. Or, there was The Maze. '''
-    txt +='''Such a thing was risky, but it offered a real test. '''
-    txt +='''Injury was likely, death a possibility in the game known as The Maze; '''
-    txt += '''if he could survive that, maybe he would be ready.... '''
+   
 
     #build strip_chr string
     strip_chr = '!@#$%^&*()_+-=<>'
@@ -54,5 +43,5 @@ def build_word_list(input_txt):
     #return finished list
     return(word_list)
 
-test = build_word_list('')
+test = build_word_list(txt)
 print(test)
